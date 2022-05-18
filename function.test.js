cef42733c3205e3dbb21c7875d4abdf2af30d9d5
Adrian Calero <acalerobarrero@gmail.com>
2022-05-18 19:24:16 +0200

    Add test but can't know use throw

--- function.test.js
+++ function.test.js
@@ -12,8 +12,16 @@ describe('Given the function pop', () => {
     describe('When receive a normal array', () => {
         test('Then should be length - 1 ', () => {
             const normalArr = [1, 2, 3];
+            pop(normalArr);
 
-            expect(pop(normalArr)).toBe(1, 2);
+            expect(normalArr).toContain(1, 2);
+        });
+    });
+    describe('When receive a string', () => {
+        test('Then should be Error', () => {
+            const string = 'pepe';
+            pop(string);
+            expect(() => throw new Error()).toThrow();
         });
     });
 });
